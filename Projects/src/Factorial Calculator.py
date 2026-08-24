581efb476b0512cf089050bb7f25beff142c3c35
poornachandra <deepakjabin407@gmail.com>
2026-08-24 19:26:05 +0530

    update code

---
+++ Projects/src/Factorial Calculator.py
@@ -0,0 +1,10 @@
+print("Factorial Calculator")
+
+number = int(input("Enter a number: "))
+
+factorial = 1
+
+for i in range(1, number + 1):
+    factorial = factorial * i
+
+print("Factorial:", factorial)
