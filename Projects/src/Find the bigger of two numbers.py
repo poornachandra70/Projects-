f0798ba0e065e344b4098afd96d5213c72cbb5b5
poornachandra <deepakjabin407@gmail.com>
2026-08-25 19:37:08 +0530

    update code

---
+++ Projects/src/Find the bigger of two numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is bigger")
+elif b > a:
+    print("Second number is bigger")
+else:
+    print("Both numbers are equal")
