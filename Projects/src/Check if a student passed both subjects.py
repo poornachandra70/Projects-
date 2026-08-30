c4edb04ca7f98ac86895f716f8cb69bac4911c9b
poornachandra <deepakjabin407@gmail.com>
2026-08-30 13:02:43 +0530

    update code

---
+++ Projects/src/Check if a student passed both subjects.py
@@ -0,0 +1,7 @@
+math = int(input("Enter Maths marks: "))
+science = int(input("Enter Science marks: "))
+
+if math >= 35 and science >= 35:
+    print("Student passed both subjects")
+else:
+    print("Student failed")
