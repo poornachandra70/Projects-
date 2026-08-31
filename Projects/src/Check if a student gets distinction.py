40f5846d5aac020422411b9182791c0305f965dd
poornachandra <deepakjabin407@gmail.com>
2026-08-31 22:21:04 +0530

    update code

---
+++ Projects/src/Check if a student gets distinction.py
@@ -0,0 +1,7 @@
+marks = int(input("Enter marks: "))
+attendance = int(input("Enter attendance: "))
+
+if marks >= 75 and attendance >= 80:
+    print("Student got distinction.")
+else:
+    print("No distinction.")
