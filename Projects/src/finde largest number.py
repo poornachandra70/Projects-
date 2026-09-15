3df7710d629d0963fc2b0f21fb89698822c30115
poornachandra <deepakjabin407@gmail.com>
2026-09-15 22:04:28 +0530

    update code

---
+++ Projects/src/finde largest number.py
@@ -0,0 +1,7 @@
+# Create an array
+numbers = [15, 45, 25, 60, 30, 3546, 353646, 35643747, 46, 46346, 34645754, 9999999999999999999]
+
+# Find largest number
+largest = max(numbers)
+
+print("Largest =", largest)
