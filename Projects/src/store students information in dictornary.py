c590d51b9dfb0e4679daa88ccd2af36aef402895
poornachandra <deepakjabin407@gmail.com>
2026-09-22 18:03:07 +0530

    update code

---
+++ Projects/src/store students information in dictornary.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Rahul",
+    "age": 21,
+    "course": "CSE",
+    "marks": 85
+}
+
+print("Name:", student["name"])
+print("Course:", student["course"])
+print("Marks:", student["marks"])
