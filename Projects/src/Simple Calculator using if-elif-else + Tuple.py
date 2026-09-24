fc97327c7f1d11d59ae16126a452e45b6a5701cd
poornachandra <deepakjabin407@gmail.com>
2026-09-24 12:55:10 +0530

    update code

---
+++ Projects/src/Simple Calculator using if-elif-else + Tuple.py
@@ -0,0 +1,21 @@
+numbers = (20, 10)
+
+a = numbers[0]
+b = numbers[1]
+
+operator = "+"
+
+if operator == "+":
+    print("Result:", a + b)
+
+elif operator == "-":
+    print("Result:", a - b)
+
+elif operator == "*":
+    print("Result:", a * b)
+
+elif operator == "/":
+    print("Result:", a / b)
+
+else:
+    print("Invalid operator")
