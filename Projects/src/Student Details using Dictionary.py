86628695c9b757599a1caa683b90206b45f5909b
poornachandra <deepakjabin407@gmail.com>
2026-09-24 12:55:58 +0530

    update code

---
+++ Projects/src/Student Details using Dictionary.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Raju",
+    "age": 21,
+    "marks": 75
+}
+
+if student["marks"] >= 40:
+    print(student["name"], "has passed")
+else:
+    print(student["name"], "has failed")
